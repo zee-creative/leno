e936b63588293d4df79b9776c165a73043306413
Abdul Ahad <abd-ul-ahad@outlook.com>
2023-07-22 16:43:32 +0500

    motion

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 import { BsTwitter, BsLinkedin, BsFacebook } from "react-icons/bs";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function Navbar() {
   const [sideActive, setSideActive] = useState<boolean>(false);
@@ -20,17 +21,10 @@ export default function Navbar() {
 
       setSideActive(false);
     });
-  } catch (error) {
-    // Handle any errors that might occur here
-  }
+  } catch {}
 
   return (
     <>
-      <div
-        className={`absolute w-full h-screen overflow-hidden top-24 left-0 bg-[#000] opacity-50 ${
-          !sideActive && "hidden"
-        }`}
-      ></div>
       <nav className=" lg:static absolute top-0 w-full left-0">
         <div
           style={{
@@ -80,37 +74,54 @@ export default function Navbar() {
             </ul>
           </div>
         </div>
-        <div
-          data-dropdown
-          className={`${
-            !sideActive && "hidden"
-          } flex justify-center flex-col items-center absolute w-screen top-[70px] left-0 bg-[var(--background-rgb)]`}
-        >
-          <ul className="flex w-full px-4 flex-col justify-center items-center py-5">
-            {linksData?.map((e, i) => {
-              return (
-                <Link
-                  key={i}
-                  className="w-full py-2 pl-5 disable_navbar_link active_navbar_link"
-                  href={e?.link}
-                >
-                  <li className="w-full text-left">{e?.name}</li>
-                </Link>
-              );
-            })}
-          </ul>
-          <div className="flex w-full justify-center items-center space-x-5 pb-5">
-            <button className="px-2 py-2">
-              <BsTwitter size={20} color="#00acee" />
-            </button>
-            <button className="px-2 py-2">
-              <BsLinkedin size={20} color="#0077b5" />
-            </button>
-            <button className="px-2 py-2">
-              <BsFacebook size={20} color="#3b5998" />
-            </button>
-          </div>
-        </div>
+        <AnimatePresence>
+          {sideActive && (
+            <motion.div
+              key="mo_dropdown"
+              initial={{ opacity: 0, y: -100 }}
+              animate={{ opacity: 1, y: -50 }}
+              //Specified Exit animation
+              exit={{ opacity: 0, y: -100 }}
+            >
+              <div
+                data-dropdown
+                className={`${
+                  !sideActive && "hidden"
+                } flex justify-center flex-col items-center absolute w-screen top-[70px] left-0 bg-[var(--background-rgb)]`}
+              >
+                <ul className="flex w-full px-4 flex-col justify-center items-center py-5">
+                  {linksData?.map((e, i) => {
+                    return (
+                      <Link
+                        key={i}
+                        className="w-full py-2 pl-5 disable_navbar_link active_navbar_link"
+                        href={e?.link}
+                      >
+                        <li className="w-full text-left">{e?.name}</li>
+                      </Link>
+                    );
+                  })}
+                </ul>
+                <div className="flex w-full justify-center items-center space-x-5 pb-5">
+                  <button className="px-2 py-2">
+                    <BsTwitter size={20} color="#00acee" />
+                  </button>
+                  <button className="px-2 py-2">
+                    <BsLinkedin size={20} color="#0077b5" />
+                  </button>
+                  <button className="px-2 py-2">
+                    <BsFacebook size={20} color="#3b5998" />
+                  </button>
+                </div>
+                <div
+                  className={`w-full h-screen overflow-hidden top-14 left-0 bg-[#000] opacity-50 ${
+                    !sideActive && "hidden"
+                  }`}
+                ></div>
+              </div>
+            </motion.div>
+          )}
+        </AnimatePresence>
       </nav>
     </>
   );
